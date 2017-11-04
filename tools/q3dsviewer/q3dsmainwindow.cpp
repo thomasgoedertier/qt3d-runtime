0fd774263849524ef90c105699e1f10a779540d1
Laszlo Agocs <laszlo.agocs@qt.io>
2017-11-04 17:33:11 +0100

    q3dsviewer: Purge disabled slide testing code
    Use qt3dsexplorer (sic) for that, will not duplicate those tools
    here.
    
    Change-Id: I5733e6dca0030dd0c1e15fbab012f1cf721fb2b7
    Reviewed-by: Andy Nichols <andy.nichols@qt.io>

--- tools/q3dsviewer/q3dsmainwindow.cpp
+++ tools/q3dsviewer/q3dsmainwindow.cpp
@@ -35,113 +35,8 @@
 #include <QMenu>
 #include <QMessageBox>
 #include <QFileDialog>
-#include <QComboBox>
-#include <QHBoxLayout>
-#include <QVBoxLayout>
-#include <QGridLayout>
-#include <QLabel>
-#include <QPushButton>
 
 QT_BEGIN_NAMESPACE
-
-class Q3DStudioSlideWindow : public QWidget
-{
-public:
-    Q3DStudioSlideWindow(Q3DStudioWindow *view, bool component);
-
-private:
-};
-
-Q3DStudioSlideWindow::Q3DStudioSlideWindow(Q3DStudioWindow *view, bool component)
-{
-    auto pres = view->uip()->presentation();
-    QGridLayout *layout = new QGridLayout;
-    bool canChange = true;
-    QComboBox *slideSource = nullptr, *compSource = nullptr;
-
-    if (!component) {
-        QHBoxLayout *slideSelLayout = new QHBoxLayout;
-        slideSelLayout->addWidget(new QLabel(tr("Scene slide")), 2);
-        QComboBox *slideSel = new QComboBox;
-        slideSource = slideSel;
-        slideSelLayout->addWidget(slideSel, 8);
-        Q3DSGraphObject *slide = pres->masterSlide()->firstChild();
-        while (slide) {
-            slideSel->addItem(QString::fromUtf8(slide->id()));
-            if (view->sceneManager()->currentSlide() == static_cast<Q3DSSlide *>(slide))
-                slideSel->setCurrentIndex(slideSel->count() - 1);
-            slide = slide->nextSibling();
-        }
-        layout->addLayout(slideSelLayout, 0, 0, 5, 2);
-    } else {
-        QHBoxLayout *compSelLayout = new QHBoxLayout;
-        compSelLayout->addWidget(new QLabel(tr("Component")), 2);
-        QComboBox *compSel = new QComboBox;
-        compSource = compSel;
-        compSelLayout->addWidget(compSel, 8);
-        Q3DSPresentation::forAllObjectsOfType(pres->scene(), Q3DSGraphObject::Component, [=](Q3DSGraphObject *obj) {
-            compSel->addItem(QString::fromUtf8(obj->id()));
-        });
-        QHBoxLayout *compSlideSelLayout = new QHBoxLayout;
-        compSlideSelLayout->addWidget(new QLabel(tr("Slide")), 2);
-        QComboBox *compSlideSel = new QComboBox;
-        slideSource = compSlideSel;
-        compSlideSelLayout->addWidget(compSlideSel, 8);
-        auto compSlideUpdate = [=]() {
-            if (compSel->count()) {
-                compSlideSel->clear();
-                Q3DSComponentNode *comp = static_cast<Q3DSComponentNode *>(pres->object(compSel->currentText().toUtf8()));
-                Q_ASSERT(comp);
-                Q3DSGraphObject *slide = comp->masterSlide()->firstChild();
-                while (slide) {
-                    compSlideSel->addItem(QString::fromUtf8(slide->id()));
-                    if (comp->currentSlide() == static_cast<Q3DSSlide *>(slide))
-                        compSlideSel->setCurrentIndex(compSlideSel->count() - 1);
-                    slide = slide->nextSibling();
-                }
-            }
-        };
-        connect(compSel, static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged), this, [=]() { compSlideUpdate(); });
-        compSlideUpdate();
-        layout->addLayout(compSelLayout, 0, 0, 2, 2);
-        layout->addLayout(compSlideSelLayout, 3, 0, 2, 2);
-        canChange = compSel->count() > 0;
-    }
-
-    QHBoxLayout *buttons = new QHBoxLayout;
-    QPushButton *cancelBtn = new QPushButton(tr("Cancel"));
-    cancelBtn->setShortcut(QKeySequence::Cancel);
-    connect(cancelBtn, &QPushButton::clicked, [this] { close(); });
-    if (canChange) {
-        QPushButton *okBtn = new QPushButton(tr("Ok"));
-        buttons->addWidget(okBtn);
-        connect(okBtn, &QPushButton::clicked, [=]() {
-#if 0
-            Q3DSSlide *s = static_cast<Q3DSSlide *>(pres->object(slideSource->currentText().toUtf8()));
-            Q_ASSERT(s);
-            if (!component) {
-                view->sceneBuilder()->setCurrentSlide(s);
-            } else {
-                Q3DSComponentNode *c = static_cast<Q3DSComponentNode *>(pres->object(compSource->currentText().toUtf8()));
-                Q_ASSERT(c);
-                c->setCurrentSlide(s, view->sceneBuilder(), pres);
-            }
-#else
-            Q_UNUSED(slideSource);
-            Q_UNUSED(compSource);
-#endif
-            close();
-        });
-    } else {
-        Q3DSUtils::showMessage(tr("No components"));
-    }
-    buttons->addWidget(cancelBtn);
-    layout->addLayout(buttons, 8, 0, 1, 2);
-
-    setLayout(layout);
-
-    resize(500, 300);
-}
 
 Q3DStudioMainWindow::Q3DStudioMainWindow(Q3DStudioWindow *view, QWidget *parent)
     : QMainWindow(parent)
@@ -182,28 +77,6 @@ Q3DStudioMainWindow::Q3DStudioMainWindow(Q3DStudioWindow *view, QWidget *parent)
     connect(renderOnDemand, &QAction::toggled, [=]() {
         view->setOnDemandRendering(renderOnDemand->isChecked());
     });
-
-#if 0
-    viewMenu->addAction(tr("&Next slide"), [=]() {
-        Q3DSPresentation *pres = view->uip()->presentation();
-        if (pres->currentSlide() && pres->currentSlide()->nextSibling())
-            pres->setCurrentSlide(static_cast<Q3DSSlide *>(pres->currentSlide()->nextSibling()), view->sceneBuilder());
-    });
-    viewMenu->addAction(tr("&Previous slide"), [=]() {
-        Q3DSPresentation *pres = view->uip()->presentation();
-        if (pres->currentSlide() && pres->currentSlide()->previousSibling())
-            pres->setCurrentSlide(static_cast<Q3DSSlide *>(pres->currentSlide()->previousSibling()), view->sceneBuilder());
-    });
-    QMenu *enterSlideSubMenu = viewMenu->addMenu(tr("&Enter slide"));
-    enterSlideSubMenu->addAction(tr("Scene slides"), [=]() {
-        Q3DStudioSlideWindow *slideSelect = new Q3DStudioSlideWindow(view, false);
-        slideSelect->show();
-    });
-    enterSlideSubMenu->addAction(tr("Component slides"), [=]() {
-        Q3DStudioSlideWindow *slideSelect = new Q3DStudioSlideWindow(view, true);
-        slideSelect->show();
-    });
-#endif
 
     QAction *pauseAnims = viewMenu->addAction(tr("&Stop animations"));
     pauseAnims->setCheckable(true);
