c2c1f8aa68e659ca89ae79244462aa7bec9dacb4
Christian Strømme <christian.stromme@qt.io>
2018-05-03 12:55:43 +0200

    Remove queued update on EOS
    This is no longer needed as the updates are not coming from the
    animator directly.
    
    Change-Id: I699978f4cdeea5932b46d82e85bd917a5c7129c9
    Reviewed-by: Laszlo Agocs <laszlo.agocs@qt.io>

--- src/runtime/q3dsslideplayer.cpp
+++ src/runtime/q3dsslideplayer.cpp
@@ -691,7 +691,7 @@ void Q3DSSlidePlayer::sendPositionChanged(Q3DSSlide *slide, float pos)
                 || (m_data.position == duration() && m_data.playbackRate > 0.0f));
 
     if (onEOS)
-        QMetaObject::invokeMethod(this, "onSlideFinished", Qt::QueuedConnection, Q_ARG(void *, slide));
+        onSlideFinished(slide);
 }
 
 void Q3DSSlidePlayer::updateNodeVisibility(Q3DSNode *node, bool shouldBeVisible)
@@ -740,22 +740,11 @@ void Q3DSSlidePlayer::onDurationChanged(float duration)
     Q_EMIT durationChanged(duration);
 }
 
-void Q3DSSlidePlayer::onSlideFinished(void *slide)
+void Q3DSSlidePlayer::onSlideFinished(Q3DSSlide *slide)
 {
     Q_ASSERT(m_data.slideDeck);
-
-    // The call to onSlideFinished is queued to avoid re-entrance issues, so make sure
-    // we're still operating and that the slide that finish is still current!
-    if (m_data.state != PlayerState::Playing)
-        return;
-
-    Q3DSSlide *currentSlide = m_data.slideDeck->currentSlide();
-    if (currentSlide != slide) {
-        qCDebug(lcSlidePlayer, "onSlideFinished called for \"%s\", but slide has already changed to \"%s!",
-                qPrintable(getSlideName(static_cast<Q3DSSlide *>(slide))), qPrintable(getSlideName(currentSlide)));
-        return;
-    }
-
+    Q_ASSERT(m_data.state == PlayerState::Playing);
+    Q_ASSERT(slide == m_data.slideDeck->currentSlide());
 
     // We don't change slides automatically in Editor mode
     if (m_mode == PlayerMode::Editor) {
@@ -764,7 +753,7 @@ void Q3DSSlidePlayer::onSlideFinished(void *slide)
     }
 
     // Get the slide's play mode
-    const auto playMode = currentSlide->playMode();
+    const auto playMode = slide->playMode();
 
     PlayerState state = PlayerState::Stopped;
 
@@ -774,12 +763,12 @@ void Q3DSSlidePlayer::onSlideFinished(void *slide)
         break;
     case Q3DSSlide::PlayThroughTo:
     {
-        if (currentSlide->playThrough() == Q3DSSlide::Next) {
+        if (slide->playThrough() == Q3DSSlide::Next) {
             m_data.slideDeck->nextSlide();
-        } else if (currentSlide->playThrough() == Q3DSSlide::Previous) {
+        } else if (slide->playThrough() == Q3DSSlide::Previous) {
             m_data.slideDeck->previousSlide();
-        } else if (currentSlide->playThrough() == Q3DSSlide::Value) {
-            const auto value = currentSlide->playThroughValue();
+        } else if (slide->playThrough() == Q3DSSlide::Value) {
+            const auto value = slide->playThroughValue();
             if (value.type() == QVariant::Int) { // Assume this is a fixed index value
                 m_data.slideDeck->setCurrentIndex(value.toInt());
             } else if (value.type() == QVariant::String) { // Reference to a slide?
